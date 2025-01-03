632888f793bb29df435b84c0d23dde493a47d30a
ZainabCodes01 <zainabzahid857@gmail.com>
2025-01-03 15:42:51 -0800

    Role Base Authentication

--- routes/admin.php
+++ routes/admin.php
@@ -2,12 +2,14 @@
 
 use App\Http\Controller\DepartmentController;
 use Illuminate\Support\Facades\Route;
-Route::prefix('admin')->group(function () {
+Route::middleware('isAdmin')->prefix('admin')->group(function () {
 
     Route::get('/', function () {
         return view('admin.master.dashboard');
+        //return 'hellow admin';
     });
 
     Route::resource('departments', DepartmentController::class);
 
 });
+
